d5b728527664a903b2822171335e29b918bc7d4f
Valeriya Levchenko <levchenko_lera@mail.ru>
2022-06-30 23:20:53 +0300

    task #4 done. corrected

--- task_4/solution.js
+++ task_4/solution.js
@@ -11,7 +11,7 @@ function parseProducts(json) {
     let products; 
     products = data.products;
     // Верните как результат функции свойство products объекта data
-    return parseProducts.products
+    return data.products;
 }
 
 // Напишите функцию renderProductsCards(json)
@@ -19,15 +19,15 @@ function renderProductsCards(json) {
 
 // Аргументом функции является JSON
 // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
-clearProducts();
+    clearProducts();
 // Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
-let products;
-products = parseProducts(json);
+    let products;
+    products = parseProducts(json);
 // Запишите в переменную length значение свойства products.length
-let length = products.length;
+    let length = products.length;
 // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
-for (let i = 0; i < length; i += 1) {
-    addProduct(products[i]);
+    for (let i = 0; i < length; i += 1) {
+        addProduct(products[i]);
 // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
-}
+    }
 }
